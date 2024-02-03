cb7b8db05e6fe67137863c6fe05799d0426db2f1
matiassuarez109 <maimaxi28@hotmail.com>
2024-02-02 23:22:30 -0300

    react

--- script/main2.js
+++ script/main2.js
@@ -2,8 +2,9 @@
 
 // recuperamos el boton
 
-const button = document.querySelector('button')
+const buttons = document.querySelectorAll('button')
 
+buttons.forEach(button => {
 // al hacer click en el boton, tenemos que ejecutar una funcion
 button.addEventListener('click', function () {
     //recuperar la id del atributo HTML
@@ -19,5 +20,6 @@ button.addEventListener('click', function () {
         button.classList.add('liked')
         button.innerText = 'Quitar me gusta'
     }
+})
 })
 
---
+++ script/main3.js
@@ -0,0 +1,9 @@
+//import React from "https//esm.sh/raect@18.2.0"
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
+
+const appDomElement = document.getElementById('app')
+
+const root = ReactDOM.createRoot(appDomElement)
+//const button = React.createElement('button', {"data-id": 123 }, 'Me gusta')
+
+root.render('butthola recaton')
