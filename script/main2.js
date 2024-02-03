fb02ef0ccaa59707a6e20b358f3d12f54da735da
matiassuarez109 <maimaxi28@hotmail.com>
2024-02-02 22:23:04 -0300

    nuevo

---
+++ script/main2.js
@@ -0,0 +1,23 @@
+// vanilla JavaScript
+
+// recuperamos el boton
+
+const button = document.querySelector('button')
+
+// al hacer click en el boton, tenemos que ejecutar una funcion
+button.addEventListener('click', function () {
+    //recuperar la id del atributo HTML
+    const id = button.getAttribute('data-id')
+
+    //llamar a un servicio para actualizar si me gusta
+    // toggerLike(id)
+
+    if (button.classList.contains('liked')) {
+        button.classList.remove('liked')
+        button.innerText = 'Me gusta'
+    }else {
+        button.classList.add('liked')
+        button.innerText = 'Quitar me gusta'
+    }
+})
+
